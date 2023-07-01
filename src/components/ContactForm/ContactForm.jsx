8607e7b043caff662d42d281fbe6ed8c5338fe50
demonikos <119507967+demonikos@users.noreply.github.com>
2023-07-01 22:02:30 +0300

    doing 1-2 steps

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 
 export class ContactForm extends Component {
